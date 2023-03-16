2db7ad636917d12d31cbf6747b135e347de3bc45
NiklasHohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-03-16 17:42:59 +0100

    Suppress plots
    Suppress plots of platform margin & 3D section to speed up graphic output

--- plotFinalGraphics.m
+++ plotFinalGraphics.m
@@ -1,13 +1,13 @@
 function plotFinalGraphics(glob, stats, iteration)
 
-    platMarginTraj = calculatePlatformMarginTrajectory(glob, iteration, round(glob.xSize / 2));
+    %platMarginTraj = calculatePlatformMarginTrajectory(glob, iteration, round(glob.xSize / 2));
 
     fprintf('Drawing the dip section and the chronostrat diagram...');
     plotDipSections(glob, stats, iteration); %cross and chrono
     fprintf('Done\n');
     
-    fprintf('Drawing the 3D block diagram...');
-    plot3DBlock(glob, iteration); %cross and chrono
-    fprintf('Done\n');
+    %fprintf('Drawing the 3D block diagram...');
+    %plot3DBlock(glob, iteration); %cross and chrono
+    %fprintf('Done\n');
 end 
 
