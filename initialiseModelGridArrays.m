a0008eab7dad1f4678a7202998a61cdc2c44d572
NiklasHohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-03-17 09:01:17 +0100

    changed temp res
    Set temporal resolution to 1 kyr & maximum iterations to 3000

--- initialiseModelGridArrays.m
+++ initialiseModelGridArrays.m
@@ -2,7 +2,7 @@ function glob = initialiseModelGridArrays(glob)
 % Create the key model grid matrices here in the global data structure glob, so that they can be referenced
 % throughout the rest of initialisation and model run routines
     
-    glob.maxIts = 1005; % To allow up to 1My model duration for a reasonable 1ky time step
+    glob.maxIts = 3005; % To allow max 3000 iterations
     glob.maxFacies = 9; % Assumes up to 3 producing facies, 3 transported facies, 1 subaerial exposure, 1 pelagic and 1 spare
 
     glob.strata = zeros(glob.ySize, glob.xSize, glob.maxIts);
