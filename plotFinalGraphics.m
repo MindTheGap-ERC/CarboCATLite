3e96f29336f6795b5bdee66f1fb068ef8d8264e0
NiklasHohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-03-17 12:48:02 +0100

    autosave figure

--- plotFinalGraphics.m
+++ plotFinalGraphics.m
@@ -6,6 +6,11 @@ function plotFinalGraphics(glob, stats, iteration)
     plotDipSections(glob, stats, iteration); %cross and chrono
     fprintf('Done\n');
     
+    % autosave figure
+    disp('Saving figure')
+    exportgraphics(gcf,'dip_section_and_chronostrat.pdf','ContentType','vector')    
+    disp('figure saved')
+    
     %fprintf('Drawing the 3D block diagram...');
     %plot3DBlock(glob, iteration); %cross and chrono
     %fprintf('Done\n');
