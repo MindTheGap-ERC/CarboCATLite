b08f9ca7188033dbe6359b5605f3e6a645264e7b
xyl96 <64159957+xyl96@users.noreply.github.com>
2023-08-08 17:02:43 +0200

    plotfinalfaciesmap
    this code plots the final bathymetry and facies maps after 2000 iterations.

---
+++ plotFinalFaciesMap.m
@@ -0,0 +1,27 @@
+%%% find the bathymetry
+wd = round(glob.wd(:,:,2000));
+
+testX = 1:glob.xSize;
+testY = 1:glob.ySize;
+
+    for y=1:glob.ySize
+        for x=1:glob.xSize
+            faciesM(y,x)=glob.facies{y,x,wd(y,x)+1}(1);
+        end
+    end
+    figure;
+    p = pcolor(testX, testY, double(faciesM));
+    colormap(glob.faciesColours);
+    colorbar;
+    figure;
+    colorfacies = double(faciesM);
+    surf(testX,testY,-wd,colorfacies);
+    colormap(glob.faciesColours);
+    colorbar;
+    set(p,'LineStyle','none');
+    view([0 90]);
+    xlabel('X Distance (grid points)');
+    ylabel('Y Distance (grid points)');
+    axis square
+    view([135, 45]);
+    title('final surface facies distribution');
